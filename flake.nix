8d7099fefb7cfa9ed8b24b21f100e09778ab5d06
Jonas Kruckenberg <iterpre@protonmail.com>
2026-01-27 13:05:16 +0100

    wip

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Simple flake with a devshell";
+
+  # Add all your dependencies here
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    blueprint = {
+      url = "github:numtide/blueprint";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  # Load the blueprint
+  outputs = inputs: inputs.blueprint { inherit inputs; };
+}
--- hosts/ardmore/services/tailscale.nix
+++ hosts/ardmore/services/tailscale.nix
@@ -2,7 +2,7 @@
 {
   services.tailscale = {
     enable = true;
-    extraSetFlags = [ "--netfilter-mode=nodivert" ];
+  extraSetFlags = [ "--netfilter-mode=nodivert" ];
   };
   networking.firewall = {
     trustedInterfaces = [ "tailscale0" ];

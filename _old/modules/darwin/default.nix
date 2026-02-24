ea718fd07c8fa8e151a4c6c5281c5d0b0090cd50
JonasKruckenberg <iterpre@protonmail.com>
2026-02-24 11:33:20 +0100

    remove old

--- _old/modules/darwin/default.nix
+++
@@ -1,4 +0,0 @@
-_: {
-  system-defaults = import ./system-defaults.nix;
-  user = import ./user.nix;
-}
--- _old/modules/default.nix
+++
@@ -1,8 +0,0 @@
-{ inputs, flake, ... }:
-{
-  flake = {
-    commonModules = import ./common { inherit inputs flake; };
-    darwinModules = import ./darwin { inherit inputs; };
-    nixosModules = import ./nixos { inherit inputs; };
-  };
-}

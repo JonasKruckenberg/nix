e420b039b89b43a9c966917458de6336675590b1
JonasKruckenberg <iterpre@protonmail.com>
2026-03-02 09:41:54 +0100

    disable caddy for now

--- services/grafana.nix
+++ services/grafana.nix
@@ -1,21 +1,21 @@
 { config, ... }:
 {
-  services.caddy.enable = true;
+  # services.caddy.enable = true;
 
-  services.caddy.virtualHosts = {
-    "https://${config.services.grafana.settings.server.domain}" = {
-      extraConfig = ''
-        # This will create a new node at grafana.$TAILNET_NAME.ts.net
-        bind tailscale/grafana
-        # Enables the Tailscale authentication provider
-        tailscale_auth
-        # Forwards your Tailscale user ID to Grafana. Usually your email address.
-        reverse_proxy ${config.services.grafana.settings.server.http_addr}:${toString config.services.grafana.settings.server.http_port} {
-          header_up X-Webauth-User {http.auth.user.tailscale_user}
-        }
-      '';
-    };
-  };
+  # services.caddy.virtualHosts = {
+  #   "https://${config.services.grafana.settings.server.domain}" = {
+  #     extraConfig = ''
+  #       # This will create a new node at grafana.$TAILNET_NAME.ts.net
+  #       bind tailscale/grafana
+  #       # Enables the Tailscale authentication provider
+  #       tailscale_auth
+  #       # Forwards your Tailscale user ID to Grafana. Usually your email address.
+  #       reverse_proxy ${config.services.grafana.settings.server.http_addr}:${toString config.services.grafana.settings.server.http_port} {
+  #         header_up X-Webauth-User {http.auth.user.tailscale_user}
+  #       }
+  #     '';
+  #   };
+  # };
 
   services.grafana = {
     enable = true;

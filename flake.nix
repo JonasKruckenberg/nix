218159eca5b4e1146947b4e490d1f542fbfc6a14
Jonas Kruckenberg <iterpre@protonmail.com>
2026-04-02 10:43:53 +0200

    use same version across nixos and nix darwin

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/master";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 

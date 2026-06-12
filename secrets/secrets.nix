be450ef3e787b3639253f6f62f3f1bf3f045983e
JonasKruckenberg <iterpre@protonmail.com>
2026-06-12 15:16:04 +0200

    use correct host key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,7 @@ let
 
   # ardmore's dedicated agenix host key (the key at age.identityPaths) lets the server decrypt
   # at activation.
-  ardmore = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILbXc4uZ30kUyGz+Z4HmIBHs9Ph3rQqRGRmsANceUvIc";
+  ardmore = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGUKx/lzVEe1tJbz4Dcx58PoNMGvQ8O3zjKiJjasDJbs";
 in
 {
   "bulletin-smtp.age".publicKeys = [
